229b15c86db56c2e57a250f9a2779fcd895d86f4
mm-masahiro <hand5.mm9.mzh@gmail.com>
2023-01-17 22:35:49 +0900

    create daily review script

--- create-daily-review-template.sh
+++ create-daily-review-template.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-today=`date "+%Y-%m-%d"`
+today=$(date "+%Y-%m-%d")
+thisMonth=$(date "+%Y-%m")
 
 echo $today
 
-cp daily-review-template.md $today-daily-review.md
+cp daily-review-template.md ./daily-review/$thisMonth/$today-daily-review.md
---
+++ create-daily-review.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+./create-this-month-dir.sh
+./create-daily-review-template.sh
